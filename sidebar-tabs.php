8ed312fd826a0b4de60c2dbdf63eca3819b9d689
Matt Beall <mbeall@starverte.com>
2013-08-20 23:50:15 -0600

    Clean up spacing

--- sidebar-tabs.php
+++ sidebar-tabs.php
@@ -7,35 +7,35 @@
  */
 ?>
 
-		<div id="tabs" class="widget-area" role="complementary">
-			<aside id="tab1">
-					<?php
-						$settings = get_option('lifepointe_sidetabs');
-						$tab1_value = $settings['tab1'];
-						echo '<a class="lbpModal" href="' . get_permalink( $tab1_value ) . '" title="' . esc_attr( $tab1_value->post_title ) . '">'; ?>
-						<img src="<?php echo get_template_directory_uri(); ?>/images/resourcestab.png" />
-					<?php	echo '</a>';
-					?>	
-			</aside>
+    <div id="tabs" class="widget-area" role="complementary">
+      <aside id="tab1">
+          <?php
+            $settings = get_option('lifepointe_sidetabs');
+            $tab1_value = $settings['tab1'];
+            echo '<a class="lbpModal" href="' . get_permalink( $tab1_value ) . '" title="' . esc_attr( $tab1_value->post_title ) . '">'; ?>
+            <img src="<?php echo get_template_directory_uri(); ?>/images/resourcestab.png" />
+          <?php  echo '</a>';
+          ?>  
+      </aside>
             
-            		<aside id="tab2">
-					<?php
-						$settings = get_option('lifepointe_sidetabs');
-						$tab2_value = $settings['tab2'];
-						echo '<a class="lbpModal" href="' . get_permalink( $tab2_value ) . '" title="' . esc_attr( $tab2_value->post_title ) . '">'; ?>
-						<img src="<?php echo get_template_directory_uri(); ?>/images/prayertab.png" />
-					<?php	echo '</a>';
-					?>	
-			</aside>
+                <aside id="tab2">
+          <?php
+            $settings = get_option('lifepointe_sidetabs');
+            $tab2_value = $settings['tab2'];
+            echo '<a class="lbpModal" href="' . get_permalink( $tab2_value ) . '" title="' . esc_attr( $tab2_value->post_title ) . '">'; ?>
+            <img src="<?php echo get_template_directory_uri(); ?>/images/prayertab.png" />
+          <?php  echo '</a>';
+          ?>  
+      </aside>
             
-            		<aside id="tab3">
-					<?php
-						$settings = get_option('lifepointe_sidetabs');
-						$tab3_value = $settings['tab3'];
-						echo '<a class="lbpModal" href="' . get_permalink( $tab3_value ) . '" title="' . esc_attr( $tab3_value->post_title ) . '">'; ?>
-						<img src="<?php echo get_template_directory_uri(); ?>/images/contacttab.png" />
-					<?php	echo '</a>';
-					?>	
-			</aside>            
+                <aside id="tab3">
+          <?php
+            $settings = get_option('lifepointe_sidetabs');
+            $tab3_value = $settings['tab3'];
+            echo '<a class="lbpModal" href="' . get_permalink( $tab3_value ) . '" title="' . esc_attr( $tab3_value->post_title ) . '">'; ?>
+            <img src="<?php echo get_template_directory_uri(); ?>/images/contacttab.png" />
+          <?php  echo '</a>';
+          ?>  
+      </aside>            
             
-		</div><!-- #tabs .widget-area -->
+    </div><!-- #tabs .widget-area -->
