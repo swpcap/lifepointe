e726469d1ac6a4951bbbb02b1ac567fc251f9ecb
Matt Beall <mbeall@starverte.com>
2013-08-20 23:44:42 -0600

    Clean up spacing

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <?php global  $post;
 if  ($post->post_parent && post_password_required($post->post_parent))
-	wp_redirect(get_permalink($post->post_parent));?>
+  wp_redirect(get_permalink($post->post_parent));?>
 <?php
 /**
  * The Header for our theme.
@@ -28,34 +28,34 @@ if  ($post->post_parent && post_password_required($post->post_parent))
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width" />
 <title><?php
-	/*
-	 * Print the <title> tag based on what is being viewed.
-	 */
-	global $page, $paged;
+  /*
+   * Print the <title> tag based on what is being viewed.
+   */
+  global $page, $paged;
 
-	wp_title( '|', true, 'right' );
+  wp_title( '|', true, 'right' );
 
-	// Add the blog name.
-	bloginfo( 'name' );
+  // Add the blog name.
+  bloginfo( 'name' );
 
-	// Add the blog description for the home/front page.
-	$site_description = get_bloginfo( 'description', 'display' );
-	if ( $site_description && ( is_home() || is_front_page() ) )
-		echo " | $site_description";
+  // Add the blog description for the home/front page.
+  $site_description = get_bloginfo( 'description', 'display' );
+  if ( $site_description && ( is_home() || is_front_page() ) )
+    echo " | $site_description";
 
-	// Add a page number if necessary:
-	if ( $paged >= 2 || $page >= 2 )
-		echo ' | ' . sprintf( __( 'Page %s', 'lifepointe' ), max( $paged, $page ) );
+  // Add a page number if necessary:
+  if ( $paged >= 2 || $page >= 2 )
+    echo ' | ' . sprintf( __( 'Page %s', 'lifepointe' ), max( $paged, $page ) );
 
-	?></title>
+  ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 <link rel="stylesheet" type="text/css" media="all" href="<?php echo get_template_directory_uri(); ?>/style-theme.php" />
 <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <?php if ( is_single('192') ): 
-	echo '<meta http-equiv="refresh" content="0; url=http://bit.ly/rFpQJi" />';
-	endif ?>
+  echo '<meta http-equiv="refresh" content="0; url=http://bit.ly/rFpQJi" />';
+  endif ?>
 <link rel="icon" 
       type="image/png" 
       href="<?php echo get_template_directory_uri(); ?>/images/icon-32.png">
@@ -69,9 +69,9 @@ if  ($post->post_parent && post_password_required($post->post_parent))
 <![endif]-->
 <script language="javascript" type="text/javascript">
 function popUp (c) {
-	window.open(c,
-		'window',
-		'width=290,height=50,scrollbars=no,status=no');
+  window.open(c,
+    'window',
+    'width=290,height=50,scrollbars=no,status=no');
 }
 </script>
 
@@ -83,41 +83,41 @@ function popUp (c) {
 <body <?php body_class(); ?>>
 <?php include_once("analyticstracking.php") ?>
 <?php if(post_type_exists('post')) : ?>
-	<div id="fb-root"></div>
-	<script>(function(d, s, id) {
-	  var js, fjs = d.getElementsByTagName(s)[0];
-	  if (d.getElementById(id)) return;
-	  js = d.createElement(s); js.id = id;
-	  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=296457183781895";
-	  fjs.parentNode.insertBefore(js, fjs);
-	}(document, 'script', 'facebook-jssdk'));</script>
+  <div id="fb-root"></div>
+  <script>(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=296457183781895";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));</script>
 <?php endif; ?>
 <div id="page" class="hfeed">
-	<header id="branding" role="banner">
-		<hgroup>
-			<h1 id="site-title"><a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 id="site-description"><?php bloginfo( 'description' ); ?></h2>
-		</hgroup>
+  <header id="branding" role="banner">
+    <hgroup>
+      <h1 id="site-title"><a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+      <h2 id="site-description"><?php bloginfo( 'description' ); ?></h2>
+    </hgroup>
         
         <aside id="search" class="widget widget_search">
-			<?php get_search_form(); ?>
-		</aside>
+      <?php get_search_form(); ?>
+    </aside>
 
-		<a href="<?php echo site_url(); ?>"><img class="logo" src="<?php echo get_template_directory_uri(); ?>/images/logo.png"></a>
+    <a href="<?php echo site_url(); ?>"><img class="logo" src="<?php echo get_template_directory_uri(); ?>/images/logo.png"></a>
 
-		<nav id="access" role="navigation">
-			<h1 class="assistive-text section-heading"><?php _e( 'Main menu', 'lifepointe' ); ?></h1>
-			<div class="skip-link screen-reader-text"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'lifepointe' ); ?>"><?php _e( 'Skip to content', 'lifepointe' ); ?></a></div>
+    <nav id="access" role="navigation">
+      <h1 class="assistive-text section-heading"><?php _e( 'Main menu', 'lifepointe' ); ?></h1>
+      <div class="skip-link screen-reader-text"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'lifepointe' ); ?>"><?php _e( 'Skip to content', 'lifepointe' ); ?></a></div>
 
-			<?php wp_nav_menu( array( 'theme_location' => 'left' ) ); ?>
-		</nav><!-- #access -->
+      <?php wp_nav_menu( array( 'theme_location' => 'left' ) ); ?>
+    </nav><!-- #access -->
 
-		<nav id="access2" role="navigation">
-			<h1 class="assistive-text section-heading"><?php _e( 'Main menu', 'lifepointe' ); ?></h1>
-			<div class="skip-link screen-reader-text"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'lifepointe' ); ?>"><?php _e( 'Skip to content', 'lifepointe' ); ?></a></div>
+    <nav id="access2" role="navigation">
+      <h1 class="assistive-text section-heading"><?php _e( 'Main menu', 'lifepointe' ); ?></h1>
+      <div class="skip-link screen-reader-text"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'lifepointe' ); ?>"><?php _e( 'Skip to content', 'lifepointe' ); ?></a></div>
 
-			<?php wp_nav_menu( array( 'theme_location' => 'right' ) ); ?>
-		</nav><!-- #access2 -->
-	</header><!-- #branding -->
+      <?php wp_nav_menu( array( 'theme_location' => 'right' ) ); ?>
+    </nav><!-- #access2 -->
+  </header><!-- #branding -->
 
-	<div id="main">
+  <div id="main">
