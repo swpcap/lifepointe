89aec1c6a333bab03caf4add824e24cd18ac4629
Matt Beall <mbeall@starverte.com>
2013-09-18 00:37:54 -0600

    Update format- templates for search

--- format-aside.php
+++ format-aside.php
@@ -5,7 +5,7 @@
  * Learn more: http://codex.wordpress.org/Post_Formats
  *
  * @package LifePointe
- * @since 0.8.0
+ * @since 0.8.4
  */
 ?>
 
@@ -14,16 +14,10 @@
     <h1 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'lifepointe' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
   </header><!-- .entry-header -->
 
-  <?php if ( is_search() ) : // Only display Excerpts for search pages ?>
-  <div class="entry-summary">
-    <?php the_excerpt(); ?>
-  </div><!-- .entry-summary -->
-  <?php else : ?>
   <div class="entry-content">
-    <?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'lifepointe' ) ); ?>
+    <?php the_content( __( 'Read more', 'lifepointe' ) ); ?>
     <?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'lifepointe' ), 'after' => '</div>' ) ); ?>
   </div><!-- .entry-content -->
-  <?php endif; ?>
 
   <footer class="entry-meta">
     <?php lifepointe_posted_on(); ?>
